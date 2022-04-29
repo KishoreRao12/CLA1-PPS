43177460d45bff2c34928e3af1fb8d115626f29f
KishoreRao12 <104643855+KishoreRao12@users.noreply.github.com>
2022-04-29 22:42:40 +0530

    Create PPS.c

---
+++ PPS.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {int m,n;
+            float c;
+            scanf("%d %d",&m,&n);
+            c=m*n;
+            printf("%.0f", (c/2));
+            return 0;
+}
